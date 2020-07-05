beba9f3abdf468f30777be7a3c688b2626a4cd59
sdimka <s_d_a@mail.ru>
2020-07-05 13:35:41 +0300

    Some changes

--- api.py
+++ api.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, jsonify
+from flask_api import status
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+user_list = [
+    {'sd': '12345'},
+    {'td': '54321'}
+]
 
 
 @app.route('/get/', methods=['GET'])
@@ -17,11 +23,15 @@ def get_list():
 
 @app.route('/user', methods=['POST'])
 def get_user():
-    un = request.args.get('user')
-    print(request.args)
+    data = request.get_json()
+    if 'login' in data and 'password' in data:
+        for a in user_list:
+            if data['login'] in a and a[data['login']] == data['password']:
+                print('Ok login!')
+                response = jsonify({'some': 'data'})
+                return response
     response = jsonify({'some': 'data'})
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return 'Record not found', status.HTTP_401_UNAUTHORIZED
 
 
 if __name__ == '__main__':
